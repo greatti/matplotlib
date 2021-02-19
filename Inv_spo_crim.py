5625f42d46bbffe67f4940305f6cf14c07a428f8
greatti <greatti@usp.br>
2021-02-19 18:42:59 -0300

    Created the base of my study
    I created 4 plot with mt database inv.csv, now i need to search for more information to complement my graphs and dig pandas/matplotlib/seaborn to bring a better face for my plots

--- Inv_spo_crim.py
+++ Inv_spo_crim.py
@@ -10,21 +10,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Teremos que criar 7 dataframes de duas colunas e 12 linhas
 
-record1 = pd.Series({'Name' : 'Alice', 
-                         'Class' : 'Physics',
-                         'Score' : 85})
-record2 = pd.Series({'Name' : 'Jack', 
-                         'Class' : 'Chemistry',
-                         'Score' : 82})
-record3 = pd.Series({'Name' : 'Helen', 
-                         'Class' : 'Biology', 
-                         'Score' : 90})
-''' aqui temos informações sobre 3 pessoas, com suas respectivas materias e notas
-para juntar as 3 series precisamos chamar uma função chamada DataFrame, geralmente representada por df
-'''
-df = pd.DataFrame([record1, record2, record3], 
-                  index = ['school1', 'school2', 'school3'])
-''' veja que adicionamos indexes arbitrarios porque nao queriamos os padroes 0 1 2 '''
-print(df)
+sns.set_style('white') #vamos mudar o estilo
+
+df = pd.read_csv('C:/Users/great/Desktop/py4e/Plotting/inv.csv')
+
+df= df.rename(columns = {'Invest. Esp' : 'INVEST'})
+df.ANO = pd.to_numeric(df.ANO , errors='coerce')
+df.INVEST = pd.to_numeric(df.INVEST , errors='coerce')
+
+df = df.set_index('ANO')
+print(df.head())
+print(df.columns)
+
+
+df.plot.line(y = 'Crim/100mil', x = 'INVEST',
+             title = 'A influência do investimento em esportes em Maringá/PR na criminalidade anual.',
+             ylabel = 'Criminalidade a cada  100mil Habitantes',
+             grid = True,  
+             );
+
+df.plot.line(y = 'Bolsas_Atleta', x = 'INVEST',
+             title = 'A influência do investimento em esportes em Maringá/PR na criminalidade anual.',
+             ylabel = 'Criminalidade a cada  100mil Habitantes',
+             grid = True,  
+             );
+
+
+titles = ['Criminalidade ao longo dos anos', 'Investimento ao longo dos anos']
+print(len(df.columns))
+
+df.plot(kind = 'line', subplots = True, grid = True, title = 'Dados por Ano', 
+        layout = (1,3), sharex = True, legend = True);
+
+
+
+
+
+
+
+
+
