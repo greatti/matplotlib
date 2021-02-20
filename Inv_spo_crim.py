3df82623b127fe2f0203d8c4c8135456e74bc415
greatti <greatti@usp.br>
2021-02-20 09:46:19 -0300

    Update in plots
    I gave them a little more style

--- Inv_spo_crim.py
+++ Inv_spo_crim.py
@@ -23,25 +23,38 @@ df = df.set_index('ANO')
 print(df.head())
 print(df.columns)
 
-
-df.plot.line(y = 'Crim/100mil', x = 'INVEST',
-             title = 'A influência do investimento em esportes em Maringá/PR na criminalidade anual.',
-             ylabel = 'Criminalidade a cada  100mil Habitantes',
-             grid = True,  
-             );
-
-df.plot.line(y = 'Bolsas_Atleta', x = 'INVEST',
-             title = 'A influência do investimento em esportes em Maringá/PR na criminalidade anual.',
-             ylabel = 'Criminalidade a cada  100mil Habitantes',
-             grid = True,  
-             );
-
-
 titles = ['Criminalidade ao longo dos anos', 'Investimento ao longo dos anos']
 print(len(df.columns))
 
 df.plot(kind = 'line', subplots = True, grid = True, title = 'Dados por Ano', 
-        layout = (1,3), sharex = True, legend = True);
+        layout = (3,1), sharex = True, legend = True, 
+        colormap = 'cividis');
+
+df = df.reset_index()
+dfc = df.copy()
+del dfc['ANO']
+
+
+dfc = dfc.set_index('INVEST')
+dfc = dfc.sort_index()
+dfc = dfc.reset_index()
+
+dfc.plot.line(y = 'Crim/100mil', x = 'INVEST',
+             title = 'A influência do investimento em esportes em Maringá/PR na criminalidade anual.',
+             ylabel = 'Criminalidade a cada  100mil Habitantes',
+             grid = True,
+             colormap = 'viridis'
+             );
+
+dfc.plot.line(y = 'Bolsas_Atleta', x = 'INVEST',
+             title = 'A influência do investimento em esportes em Maringá/PR na quantidade de bolsas atleta fornecidas',
+             ylabel = 'Numero de Bolsas Atleta fornecidas',
+             grid = True,  
+             colormap = 'plasma'
+             );
+
+
+
 
 
 
